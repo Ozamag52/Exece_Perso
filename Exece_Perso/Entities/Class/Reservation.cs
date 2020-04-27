99eea34ff06bf2533025a3586172cb8d4a9d0918
Gustavo Peixoto <henriquegustavo2010@gmail.com>
2020-04-27 15:56:41 -0300

    Criacao da classe Reservation e do DomainExceptions

---
+++ Exece_Perso/Entities/Class/Reservation.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Exece_Perso.Entities.Exceptions;
+
+namespace Exece_Perso.Entities.Class
+{
+    class Reservation
+    {
+        public int RoomNumber { get; set; }
+        public DateTime CheckIn { get; set; }
+        public DateTime CheckOut { get; set; }
+
+        public Reservation()
+        {
+
+        }
+
+        public Reservation(int roomNumber, DateTime checkIn, DateTime checkOut)
+        {
+            if (checkIn < DateTime.Now)
+            {
+                throw new DomainException("The check in should be a future day");
+
+            }
+            if (checkOut <= CheckIn)
+            {
+                throw new DomainException("The check in should be a date before the check out ");
+            }
+            RoomNumber = roomNumber;
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+        }
+
+        public int Duration()
+        {
+            TimeSpan duration = CheckOut.Subtract(CheckIn);
+
+            return (int)duration.TotalDays;
+        }
+
+        public void UpdateDates(DateTime checkIn, DateTime checkOut)
+        {
+            if(checkIn < DateTime.Now)
+            {
+                throw new DomainException("The check in should be a future day");
+                   
+            }
+            if (checkOut <= CheckIn)
+            {
+                throw new DomainException("The check in should be a date before the check out ");
+            }
+        }
+    }
+}
